3f5f7ab4ac383ebc36b32d18706249cde550bd3a
inkwill8 <inkwill8@proton.me>
2026-02-12 16:40:41 -0600

    Add nvim-dap and nvim-dap-ui capability for cpp

---
+++ nvim/lua/plugins/debugging.lua
@@ -0,0 +1,69 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"nvim-neotest/nvim-nio",
+	},
+	config = function()
+		local dap, dapui = require("dap"), require("dapui")
+		dapui.setup({
+			layouts = {
+				{
+					elements = {
+						{ id = "scopes", size = 0.25 },
+						{ id = "breakpoints", size = 0.25 },
+						{ id = "stacks", size = 0.25 },
+						{ id = "watches", size = 0.25 },
+					},
+					size = 35, -- width of the left panel
+					position = "left",
+				},
+				{
+					elements = {
+						{ id = "repl", size = 0.75 },
+						{ id = "console", size = 0.25 },
+					},
+					size = 15, -- height of the bottom panel
+					position = "bottom",
+				},
+			},
+		})
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		-- C++ Specific Configs
+		dap.configurations.cpp = {
+			{
+				name = "Launch",
+				type = "lldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+				args = {},
+			},
+		}
+
+		vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+		vim.keymap.set("n", "<Leader>dx", dapui.toggle, {})
+		dap.adapters.lldb = {
+			type = "executable",
+			command = "/usr/bin/lldb-dap", -- adjust as needed, must be absolute path
+			name = "lldb",
+		}
+	end,
+}
