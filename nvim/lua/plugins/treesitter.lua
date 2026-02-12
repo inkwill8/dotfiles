c046385537bfcda8c75b8bffd3f451f08e9dd8c3
inkwill8 <inkwill8@proton.me>
2026-02-12 15:04:46 -0600

    Add linters to config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,21 +1,21 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function()
-      require'nvim-treesitter'.install { 'lua', 'javascript', 'cpp', 'c', 'html', 'css', 'bash' }
-      
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = { 'lua', 'javascript', 'cpp', 'c', 'html', 'css', 'bash' },
-        callback = function() 
-          vim.treesitter.start() 
-        end,
-      })
-      
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = { 'lua', 'javascript', 'cpp', 'c', 'html', 'css', 'bash' },
-        callback = function()
-          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end,
-      })
-    end
-  }
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter").install({ "lua", "javascript", "cpp", "c", "html", "css", "bash", "ruby" })
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "lua", "javascript", "cpp", "c", "html", "css", "bash", "ruby" },
+			callback = function()
+				vim.treesitter.start()
+			end,
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "lua", "javascript", "cpp", "c", "html", "css", "bash", "ruby" },
+			callback = function()
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
+	end,
+}
